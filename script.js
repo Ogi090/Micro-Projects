9e07b5b285f385199301b300e223bfd4eedee0e0
ogi90 <todorovicnenadtn@gmail.com>
2023-06-16 21:36:30 +0200

    Added modifications

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+// Try again from the top
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playRound(playerSelection, getComputerChoice) {
+
+    // playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === getComputerChoice) {
+        return "It's a tie!";
+    }
+
+    if ((playerSelection === 'Rock' && getComputerChoice === 'Scissors') || (playerSelection === 'Paper' && getComputerChoice === 'Rock') || (playerSelection === 'Scissors' && getComputerChoice === 'Paper')) {
+
+        return 'Player 1 wins!';
+    } else {
+        return 'Player 2 wins!';
+    }
+}
+
+let answer = parseInt(prompt("Please choise?"));
+let playerSelection = answer;
+
+
 
 // Computer choise
 function getComputerChoice(choise) {
@@ -12,38 +72,52 @@ let computerSelection = getComputerChoice(choise);
 console.log(computerSelection);
 
 
-
-function playRound(playerSelection, getComputerChoice) {
-
-    // playerSelection.toLowerCase();
-    // getComputerChoice.toLowerCase();
-
-    if (playerSelection === getComputerChoice) {
-        return "It's a tie!";
-    }
-
-    if ((playerSelection === 'Rock' && getComputerChoice === 'Scissors') || (playerSelection === 'Paper' && getComputerChoice === 'Rock') || (playerSelection === 'Scissors' && getComputerChoice === 'Paper')) {
-
-        return 'Player 1 wins!';
-    } else {
-        return 'Player 2 wins!';
-    }
-
-    // if (getComputerChoice === "Scissors")  {
-    //     return 'Player 1 wins!';
-    // } else (getComputerChoice === "Paper") 
-    //     return 'Player 2 wins!'; 
-}
-
-let answer = parseInt(prompt("Please choise?"));
-let playerSelection = answer;
-
 console.log(playerSelection);
 console.log(playRound(playerSelection, getComputerChoice));
 
 
 
-// Working one
+
+
+
+
+// Working version 1 (doesnt inclued game())
+// function playRound(playerChoice, computerChoice) {
+//     playerChoice = playerChoice.toLowerCase();
+  
+//     if (playerChoice === computerChoice) {
+//       return "It's a tie!";
+//     } else if (
+//       (playerChoice === 'rock' && computerChoice === 'scissors') ||
+//       (playerChoice === 'paper' && computerChoice === 'rock') ||
+//       (playerChoice === 'scissors' && computerChoice === 'paper')
+//     ) {
+//       return 'You win!';
+//     } else {
+//       return 'Computer wins!';
+//     }
+//   }
+  
+//   function getRandomChoice() {
+//     const choices = ['rock', 'paper', 'scissors'];
+//     const randomIndex = Math.floor(Math.random() * choices.length);
+//     return choices[randomIndex];
+//   }
+  
+//   function startGame() {
+//     const playerChoice = prompt("Choose: rock, paper, or scissors");
+//     const computerChoice = getRandomChoice();
+//     const result = playRound(playerChoice, computerChoice);
+
+//     console.log(`Player chose: ${playerChoice}`);
+//     console.log(`Computer chose: ${computerChoice}`);
+//     console.log(result);
+//   }
+  
+//   startGame();
+
+
+/* // Working version 2 (include game())
 function playRound(playerChoice, computerChoice) {
     playerChoice = playerChoice.toLowerCase();
   
@@ -66,42 +140,40 @@ function playRound(playerChoice, computerChoice) {
     return choices[randomIndex];
   }
   
-  function startGame() {
-    const playerChoice = prompt("Choose: rock, paper, or scissors");
-    const computerChoice = getRandomChoice();
-    const result = playRound(playerChoice, computerChoice);
-
-    console.log(`Player chose: ${playerChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-    console.log(result);
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      console.log(`Round ${round}:`);
+  
+      const playerChoice = prompt("Choose: rock, paper, or scissors");
+      const computerChoice = getRandomChoice();
+      const result = playRound(playerChoice, computerChoice);
+  
+      console.log(`Player chose: ${playerChoice}`);
+      console.log(`Computer chose: ${computerChoice}`);
+      console.log(result);
+  
+      if (result === 'You win!') {
+        playerScore++;
+      } else if (result === 'Computer wins!') {
+        computerScore++;
+      }
+  
+      console.log(`Player Score: ${playerScore}`);
+      console.log(`Computer Score: ${computerScore}`);
+      console.log('------------------');
+    }
+  
+    if (playerScore > computerScore) {
+      console.log('Congratulations! You win the game!');
+    } else if (playerScore < computerScore) {
+      console.log('Computer wins the game. Better luck next time!');
+    } else {
+      console.log("It's a tie! The game ends in a draw.");
+    }
   }
   
-  startGame();
-
-
-
-
-
-// Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
-
-/* 
-    1. Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+  game();
 */
-
-/*  
-    2. Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-*/
-
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-/*
-    Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-    You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-    At this point you should be using console.log() to display the results of each round and the winner at the end.
-    Use prompt() to get input from the user. Read the docs here if you need to.
-    Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-    Feel free to create more “helper” functions if you think it would be useful..
-*/
-
-
-
